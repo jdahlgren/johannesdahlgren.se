d81cf75dd28dec2ea30b8d390fa99846b9d65f69
johda876 <johda876@student.liu.se>
2014-03-10 23:49:59 +0100

    Menystruktur klar

--- js/app.js
+++ js/app.js
@@ -1,1 +1,50 @@
-angular.module('jdApp', []);
+var app = angular.module('jdApp', ['ngRoute']);
+
+app.controller('HomeCtrl', function($scope){
+	$scope.title = 'Johannes Dahlgren';	
+	$scope.lorem = 'Lorem ipsum dolor sit amet, \
+		consectetur adipiscing elit. Aliquam enim felis, \
+		imperdiet vitae accumsan quis, laoreet quis velit. \
+		Vestibulum ac.';
+});
+
+app.controller('ProjectsCtrl', function($scope){
+	$scope.projectsTitle = 'Projects here';	
+});
+app.controller('CvCtrl', function($scope){
+	$scope.cvTitle = 'CV here';
+});
+app.controller('ContactCtrl', function($scope){
+	$scope.contactTitle = 'Contact here';
+});
+
+app.controller('HeaderCtrl', function($scope, $location){
+	$scope.isActive = function (viewLocation) { 
+        return viewLocation === $location.path();
+    };
+});
+
+
+
+app.config(['$routeProvider', function($routeProvider){
+	$routeProvider.
+		when('/projects', {
+			templateUrl: 'views/projects.html',
+			controller: 'ProjectsCtrl'
+		}).
+		when('/cv', {
+			templateUrl: 'views/cv.html',
+			controller: 'CvCtrl'
+		}).
+		when('/contact', {
+			templateUrl: 'views/contact.html',
+			controller: 'ContactCtrl'
+		}).
+		when('/', {
+			templateUrl: 'views/home.html',
+			controller: 'HomeCtrl'
+		}).
+		otherwise({
+			redirectTo: '/'
+		});
+}]);
--- js/controllers/JdCtrl.js
+++
@@ -1,3 +0,0 @@
-angular.module('jdApp').controller('JdCtrl', function($scope){
-	$scope.title = 'Johannes Dahlgren';
-});
