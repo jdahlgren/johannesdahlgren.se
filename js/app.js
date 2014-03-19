b90b3b01ceb488d3f2b8e39fe11c1eed755c7706
johda876 <johda876@student.liu.se>
2014-03-19 22:51:15 +0100

    removed unused files. Moved contact to home. Changed layou in cv

--- js/app.js
+++ js/app.js
@@ -37,26 +37,6 @@ app.controller('ProjectsCtrl', function($scope, Json){
 	}
 });
 
-app.controller('ProjectDetailCtrl', function($scope, $routeParams, Json){
-	$scope.id = $routeParams.id;
-	Json.getJson('data/projects.json').success(function(data){
-		for (var i = 0; i < data.length; i++) {
-			$scope.lookup[data[i].id] = data[i];
-		}
-		$scope.project = $scope.lookup[$scope.id];
-	});
-	$scope.lookup = {};	
-	
-	$scope.project = $scope.lookup[$scope.id];
-	$scope.test = function(){
-		if($scope.project === undefined){
-			return true;
-		}
-		return false;
-	}
-	
-});
-
 app.controller('CvCtrl', function($scope, $http, Json){
 	$scope.cvTitle = 'CV here';	
 	Json.getJson('data/cv.json').success(function(data){
@@ -69,25 +49,8 @@ app.controller('CvCtrl', function($scope, $http, Json){
 	
 });
 
-app.controller('ContactCtrl', function($scope, $http, Json){
-	$scope.contactTitle = 'Contact me:';
-	Json.getJson('data/contact.json').success(function(data){
-		$scope.socialMedia = data;
-	});
-	$scope.orderProp = 'id';
-	
-	$scope.open = function(url){
-		if(url.indexOf('mailto') === 0){
-			window.location.href = url;
-		}else{
-			window.open(url);
-		}
-	}
-});
-
 app.controller('HeaderCtrl', function($scope, $location){
-	$scope.isActive = function (viewLocation) { 
-		//return viewLocation === $location.path();
+	$scope.isActive = function (viewLocation) {
 		return viewLocation === '/'+$location.path().split('/')[1];
     };
 });
@@ -98,18 +61,6 @@ app.controller('CarouselCtrl', function($scope) {
 								{image:'data/paperpilot.png'},
 								{image:'data/ballista.png'},
 								{image:'data/facerecognition.jpg'}];
-  //var slides = $scope.slides = [];
-  /*$scope.addSlide = function() {
-    var newWidth = 800 + slides.length;
-    slides.push({
-      image: 'http://lorempixel.com/' + newWidth + '/400',
-      text: ['such','wow','dat','much'][slides.length % 4] + ' ' +
-        ['meow', 'rawr', 'doge', 'nice', 'very'][slides.length % 5]
-    });
-  };
-  for (var i=0; i<10; i++) {
-    $scope.addSlide();
-  }*/
 });
 
 
@@ -123,17 +74,9 @@ app.config(['$routeProvider', function($routeProvider){
 			templateUrl: 'views/cv.html',
 			controller: 'CvCtrl'
 		}).
-		when('/contact', {
-			templateUrl: 'views/contact.html',
-			controller: 'ContactCtrl'
-		}).
 		when('/', {
 			templateUrl: 'views/home.html',
 			controller: 'HomeCtrl'
-		}).
-		when('/projects/:id',{
-			templateUrl: 'views/details.html',
-			controller: 'ProjectDetailCtrl'
 		}).
 		otherwise({
 			redirectTo: '/'
